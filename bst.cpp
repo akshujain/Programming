749e2bd37a2e5cd832c64a81b47bbabd83ff55c0
akshujain <45899855+akshujain@users.noreply.github.com>
2018-12-15 22:17:30 +0530

    Update bst.cpp

--- bst.cpp
+++ bst.cpp
@@ -31,6 +31,20 @@ Node* insert(Node* root,int data) {
 	return root;
 	}
 
+bool SearchNode(Node* root,int data) {
+    if(root == NULL){
+        return false;
+    }
+    else if(root->data == data) {
+        return true;
+    }
+    else if(data <= root->data) {
+        return SearchNode(root->left,data);
+    }
+    else {
+        return SearchNode(root->right,data);
+    }
+}
 int main(){
 	Node* root;
 	root = insert(root,10);
@@ -38,5 +52,14 @@ int main(){
 	root = insert(root ,20);
 	root = insert(root , 25);
 	Inorder(root);
+	int number;
+	cout<<"Enter a number";
+	cin>>number;
+	if(SearchNode(root,number)==true) {
+	    cout<<"found";
+	}
+        else {
+	      cout<<"Data not found";
+        }
 	return 0;
 	}
